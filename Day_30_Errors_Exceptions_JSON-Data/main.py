eea1810af2638d16aa6ba6bf57f5549db62cd862
Memphis171 <johnfmckissack@gmail.com>
2025-11-01 16:19:32 -0500

    learned how to use exceptions

---
+++ Day_30_Errors_Exceptions_JSON-Data/main.py
@@ -0,0 +1,27 @@
+#Four types of common errors: type,key, file, index
+
+# How to handle exceptions:
+# try: what you  want to ddo that might cause  an exception
+# except: what to do if there is an exception
+# else: if no exception  what next
+# finally: do this no matter what if there is an exception or not
+
+
+#Here are some ways to handles errors
+try:
+    file = open("test.txt")
+    dictionary = {"key": "value"}
+    print(dictionary["error"])
+except FileNotFoundError:
+    file = open("test.txt", "w")
+    file.write("write something")
+except KeyError as error:
+    print(f"That key {error}doesn't exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File closed.")
+
+# you could also use the raise function to force an exception when  the code won't break but it shouldn't be an option
